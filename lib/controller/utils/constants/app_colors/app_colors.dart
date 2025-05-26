642aed5c06cbef4edc032df81d169a6f856371a5
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-26 19:43:04 +0500

    cd mc

---
+++ lib/controller/utils/constants/app_colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  Color primaryColor = Colors.blue;
+  Color secondrayColor = Colors.orange;
+  Color deepColor = Colors.deepOrange;
+  Color greyColor = Colors.blueGrey;
+}
