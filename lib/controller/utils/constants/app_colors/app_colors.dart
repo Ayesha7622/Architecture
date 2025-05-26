993b806f0335c449dfbad0fc8cd815bcebeb204a
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-26 20:07:14 +0500

    jffvnd

--- lib/controller/utils/constants/app_colors/app_colors.dart
+++ lib/controller/utils/constants/app_colors/app_colors.dart
@@ -7,6 +7,7 @@ class AppColors {
   Color greyColor = Colors.blueGrey;
   Color blackColor = Colors.black;
   Color whiteColor = Colors.white;
+  Color blueAccentColor = Colors.blueAccent;
   static Color indianRedColor = Color(0xffCD5C5C);
   static Color lightCoralColor = Color(0xffF08080);
 }
