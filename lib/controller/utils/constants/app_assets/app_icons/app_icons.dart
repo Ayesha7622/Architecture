998d48411421d857513dfe54bac15c4744b2474c
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-27 21:18:46 +0500

    sbx nsam

--- lib/controller/utils/constants/app_assets/app_icons/app_icons.dart
+++ lib/controller/utils/constants/app_assets/app_icons/app_icons.dart
@@ -2,4 +2,8 @@
 
 class AppIcons {
   static String base = 'assets/';
+  static String appLogoIcons = '${base}cat.png';
+  static String exclude = '${base}exclude.png';
+  static String dices = '${base}dices.png';
+  static String arrows = '${base}arrows.png';
 }
