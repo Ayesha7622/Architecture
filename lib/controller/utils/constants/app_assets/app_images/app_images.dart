3de270c493ef9edc4af05820654326d9e0dd2f76
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-27 22:21:39 +0500

    nvbnm

---
+++ lib/controller/utils/constants/app_assets/app_images/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static String base = 'assets/';
+  static String app = '${base}app.jpg';
+  static String bnda = '${base}bnda.jpg';
+  static String clrs = '${base}clrs.png';
+  static String five = '${base}five.jpg';
+}
